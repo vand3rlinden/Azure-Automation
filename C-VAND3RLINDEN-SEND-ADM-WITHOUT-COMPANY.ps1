95642b66871d45fd9c1a3933ec567f1ec65f3a2f
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-01-08 14:36:29 +0100

    Update C-VAND3RLINDEN-SEND-ADM-WITHOUT-COMPANY.ps1

--- C-VAND3RLINDEN-SEND-ADM-WITHOUT-COMPANY.ps1
+++ C-VAND3RLINDEN-SEND-ADM-WITHOUT-COMPANY.ps1
@@ -1,5 +1,12 @@
-#Connect Azure AD
-. .\Login-AzureAD.ps1
+#This script has been made if an attribute is missing from an object and send a ticket to the servicedesk to fix it. You can tweak this script for your needs.
+
+##Login in EXO##
+$Credentials = Get-AutomationPSCredential -Name 'C-VAND3RLINDEN-AUTOMATION'
+# import the EXO module
+Import-Module ExchangeOnlineManagement
+# Connect to ExchangeOnline
+Connect-ExchangeOnline -Credential $Credentials
+##End Login EXO##
 
 # Get admin accounts without CompanyName
 $admins = Get-AzureADUser -All $true | Where-Object {($_.UserPrincipalName -like 'adm_*') -and ($_.CompanyName -like '')} | Select-Object Displayname,UserPrincipalName
@@ -10,7 +17,7 @@ $SENDMAIL = Get-AutomationPSCredential -Name 'C-VAND3RLINDEN-SENDMAIL'
 #Send from Office 365 SMTP
 $PSEmailServer = "smtp.office365.com"
 $SMTPPort = 587
-$MailTo = "ricardo@sandbox.vand3rlinden.nl"
-$MailFrom = "svc_smtp@sandbox.vand3rlinden.nl"
+$MailTo = "servicedesk@domain.com"
+$MailFrom = "svc_smtp@domain.com"
 $MailSubject = "Admin accounts without CompanyName attribute"
 Send-MailMessage -From $MailFrom -To $MailTo -Subject $MailSubject -Body ($admins | Out-String) -UseSsl -Port $SMTPPort -Credential $SENDMAIL
