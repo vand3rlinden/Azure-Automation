304f41b7e0c35409a7399141e5c493429dd378a2
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-02-01 21:39:49 +0100

    Create C-VAND3RLINDEN-SET-GUEST-COMPANY.ps1

---
+++ C-VAND3RLINDEN-SET-GUEST-COMPANY.ps1
@@ -0,0 +1,27 @@
+#This script can be used to autofill companyName attribute for known guest organizations based on guest domain in UserPrincipalName.
+
+##Login Azure AD##
+$Credentials = Get-AutomationPSCredential -Name 'C-VAND3RLINDEN-AUTOMATION'
+# import AzureAD module
+Import-Module AzureAD
+# Connect to AzureAD
+Connect-AzureAD -Credential $Credentials
+##End Login Azure AD##
+
+#Get Company: Contoso
+$ContosoAdmins = Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -like "*contoso.com*"}
+
+#Set Company: Contoso
+Foreach ($ContosoAdmin in $ContosoAdmins){
+    Set-AzureADUser -ObjectId $ContosoAdmin.UserPrincipalname -CompanyName 'Contoso'
+}
+
+#Get Company: Fabrikam
+$FabrikamAdmins = Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -like "*fabrikam.com*"}
+
+#Set Company: DEV VAND3RLINDEN
+Foreach ($FabrikamAdmin in $FabrikamAdmins){
+    Set-AzureADUser -ObjectId $Fabrikam.UserPrincipalname -CompanyName 'Fabrikam'
+}
+
+#Rollback CompanyName: Remove-AzureADUserExtension -ObjectId $VARIABLEXX.UserPrincipalname -ExtensionName "CompanyName"
