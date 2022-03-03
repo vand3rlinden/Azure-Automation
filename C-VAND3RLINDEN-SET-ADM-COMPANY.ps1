a4fdb6d85313d19ecb321a0ff612105c51a19926
Robert Prust <robert@powershellpr0mpt.com>
2022-03-03 15:35:36 +0100

    Update C-VAND3RLINDEN-SET-ADM-COMPANY.ps1
    Fixed not using the company variable on Set-AzureADUser and only get all users once instead of once per company to increase efficiency

--- C-VAND3RLINDEN-SET-ADM-COMPANY.ps1
+++ C-VAND3RLINDEN-SET-ADM-COMPANY.ps1
@@ -3,19 +3,22 @@
 #Connect Azure AD with Runbook:
 . .\Login-AzureAD.ps1
 
-$Companies = @('Contoso','Fabrikam')
-foreach ($Company in $Companies){
-    try {
-        $Members = Get-AzureADUser -All $true -ErrorAction Stop | Where-Object {$_.DisplayName -like "*($Company Admin)"}
-        foreach ($Member in $Members){
+$Companies = @('Contoso', 'Fabrikam')
+try {
+    # Get all the users in the tenant
+    $Members = Get-AzureADUser -All $true -ErrorAction Stop
+    foreach ($Company in $Companies) {
+        $Members | Where-Object {$_.DisplayName -like "*($Company Admin)"}
+        foreach ($Member in $Members) {
             try {
-                Set-AzureADUser -ObjectId $Member.UserPrincipalname -CompanyName 'Contoso' -ErrorAction Stop
+                # Set the Company attribute
+                Set-AzureADUser -ObjectId $Member.UserPrincipalname -CompanyName $Company -ErrorAction Stop
                 #Rollback: Remove-AzureADUserExtension -ObjectId -ObjectId $Member.UserPrincipalname -ExtensionName "CompanyName"
             } catch {
                 Write-Error "Unable to Set AD properties for user '$($Member.UserPrincipalName)'.`nError: $($_.Exception.Message)"
             }
         }
-    } catch {
-        Write-Error "Unable to get Azure AD Users.`nError: $($_.Exception.Message)"
     }
+} catch {
+    Write-Error "Unable to get Azure AD Users.`nError: $($_.Exception.Message)"
 }
