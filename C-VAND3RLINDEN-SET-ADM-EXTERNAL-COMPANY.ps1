4bddcec17168be1ed13c5a83c72920d823105453
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-03-04 15:51:19 +0100

    Update C-VAND3RLINDEN-SET-ADM-EXTERNAL-COMPANY.ps1

--- C-VAND3RLINDEN-SET-ADM-EXTERNAL-COMPANY.ps1
+++ C-VAND3RLINDEN-SET-ADM-EXTERNAL-COMPANY.ps1
@@ -1,19 +1,22 @@
-#This script can be used to give internal admins from external companies more identity in your tenant
-
-#Connect Azure AD with Runbook:
-. .\Login-AzureAD.ps1
+<#
+This script can be used to give your internal admin from external vendors more idenity in your tenant to set comanyName to vendor name
+Command in: Set-AzureADUser and command out: Get-AzureADUser with the $Export variable to check the output your are changing
+Command in: Set-AzureADUser and command out: Remove-AzureADUserExtension to roll back the script
+#>
 
 #List external companies
 $Companies = @('Contoso','Wortell')
 
 #Get accounts with a DisplayName like (CompanyName Admin)
+#$Export = foreach ($Company in $Companies){ #Comment out for: Test
 foreach ($Company in $Companies){
     $Members = (Get-AzureADUser -All $true | Where-Object {$_.DisplayName -like "*($Company Admin)"}).UserPrincipalname
     
     #Set CompanyName
     foreach ($Member in $Members){
-        Set-AzureADUser -ObjectId $Member -CompanyName $Company #Set
-        #Get-AzureADUser -ObjectId $Member | Select-Object UserPrincipalname #Test
-        #Remove-AzureADUserExtension -ObjectId -ObjectId $Member.UserPrincipalname -ExtensionName "CompanyName" #Rollback
+        Set-AzureADUser -ObjectId $Member -CompanyName $Company
+        #Get-AzureADUser -ObjectId $Member | Select-Object UserPrincipalName,DisplayName #Comment out for: Test
+        #Remove-AzureADUserExtension -ObjectId -ObjectId $Member -ExtensionName "CompanyName" #Comment out for: Rollback
     }
 }
+#$Export | Export-CSV -Path "C:\Temp\file3.csv" -NoTypeInformation #Comment out for: Test
