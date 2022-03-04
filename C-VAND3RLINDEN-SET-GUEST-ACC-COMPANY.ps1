d85ba88a146b10af790836c90ac6a3eb832ee928
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-03-04 15:48:09 +0100

    Update C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1

--- C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1
+++ C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1
@@ -1,4 +1,8 @@
-#This script can be used to give your guest users more idenity in your tenant to set comanyName attribute
+<#
+This script can be used to give your guest users more idenity in your tenant to set comanyName attribute
+Command in: Set-AzureADUser and command out: Get-AzureADUser with the $Export variable to check the output your are changing
+Command in: Set-AzureADUser and command out: Remove-AzureADUserExtension to roll back the script
+#>
 
 #Connect Azure AD
 . .\Login-AzureAD.ps1
@@ -7,13 +11,15 @@
 $Companies = @('Contoso','Fabrikram')
 
 #Get guest users with UPN like: company.extension#EXT#@domain.onmicrosoft.com
+#$Export = foreach ($Company in $Companies){ #Comment out for: Test
 foreach ($Company in $Companies){
     $Members = (Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -like "*$Company.*"}).UserPrincipalname
 
     #Set CompanyName
     foreach ($Member in $Members){
-        Set-AzureADUser -ObjectId $Member -CompanyName $Company #Set
-        #Get-AzureADUser -ObjectId $Member | Select-Object UserPrincipalname #Test
-        #Remove-AzureADUserExtension -ObjectId -ObjectId $Member -ExtensionName "CompanyName" #Rollback
+        Set-AzureADUser -ObjectId $Member -CompanyName $Company
+        #Get-AzureADUser -ObjectId $Member | Select-Object UserPrincipalName,DisplayName #Comment out for: Test
+        #Remove-AzureADUserExtension -ObjectId -ObjectId $Member -ExtensionName "CompanyName" #Comment out for: Rollback
     }
 }
+#$Export | Export-CSV -Path "C:\Temp\file3.csv" -NoTypeInformation #Comment out for: Test
