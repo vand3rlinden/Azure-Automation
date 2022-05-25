c1919db0466b9b946a5092bbda452f4d77039c0b
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-05-25 10:28:54 +0200

    Update C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1

--- C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1
+++ C-VAND3RLINDEN-SET-GUEST-ACC-COMPANY.ps1
@@ -2,6 +2,13 @@
 This script can be used to give your guest users more idenity in your tenant to set comanyName attribute
 Command in: Set-AzureADUser and command out: Get-AzureADUser with the $Export variable to check the output your are changing
 Command in: Set-AzureADUser and command out: Remove-AzureADUserExtension to roll back the script
+
+Note: in most scenarios the domain name match the company name, like contoso.com. But there are examples where it does not, like: f-s.com and companyname is FourStar.
+In that scenario you could add below script for each of that company:
+$Members = (Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -like "*f-s.com*"}).UserPrincipalname
+ForEach ($Member in $Members){
+    Set-AzureADUser -ObjectId $Member.UserPrincipalname -CompanyName 'FourStar'
+}
 #>
 
 #Connect Azure AD
@@ -10,7 +17,7 @@ Command in: Set-AzureADUser and command out: Remove-AzureADUserExtension to roll
 #Guest CompanyNames
 $Companies = @('Contoso','Fabrikram')
 
-#Get guest users with UPN like: company.extension#EXT#@domain.onmicrosoft.com
+#Get guest users with UPN like: companyname.extension#EXT#@domain.onmicrosoft.com (we only take 'companyname.', because guest can have different domain extensions)
 #$Export = foreach ($Company in $Companies){ #Comment out for: Test
 foreach ($Company in $Companies){
     $Members = (Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -like "*$Company.*"}).UserPrincipalname
